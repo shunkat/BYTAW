b881dcac9c61b4daaff3cecc1ebc44474c19af31
shunkat <shunkat@icloud.com>
2022-11-19 17:40:29 +0900

    外作業pushもれ

--- app/src/main/java/com/example/bytaw/ui/alarm/AlarmFragment.kt
+++ app/src/main/java/com/example/bytaw/ui/alarm/AlarmFragment.kt
@@ -1,15 +1,20 @@
 package com.example.bytaw.ui.alarm
 
+import android.app.ProgressDialog.show
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.bytaw.R
 import com.example.bytaw.databinding.FragmentAlarmBinding
 
 class AlarmFragment : Fragment() {
@@ -38,6 +43,19 @@ class AlarmFragment : Fragment() {
 //        })
         setupListAlarm()
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding!!.alarmToolBar.inflateMenu(R.menu.alarm_menu)
+        _binding!!.alarmToolBar.setOnMenuItemClickListener {
+            showTimePickerDialog()
+            true
+        }
+    }
+    fun showTimePickerDialog() {
+        val newFragment: DialogFragment = TimePickerFragment()
+        newFragment.show(childFragmentManager,"test")
     }
 
     override fun onDestroyView() {
